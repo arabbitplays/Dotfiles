95a32ebd94f62ed3ce2f27fd5e227c7017117b7a
Oschdi <osterjakob@gmail.com>
2025-12-12 14:30:38 +0100

    sync Fr 12. Dez 14:30:23 CET 2025

--- .config/nvim/lua/oschdi/plugins/neotree.lua
+++ .config/nvim/lua/oschdi/plugins/neotree.lua
@@ -1,12 +1,21 @@
 require('neo-tree').setup({
-	close_if_last_window = true,
-  filesystem = {
-    filtered_items = {
-      visible = true,       -- show hidden files
-      hide_dotfiles = false, -- do not hide dotfiles
-      hide_gitignored = true, -- optional: still hide gitignored files
+    close_if_last_window = true,
+    filesystem = {
+        filtered_items = {
+            visible = true,       -- show hidden files
+            hide_dotfiles = false, -- do not hide dotfiles
+            hide_gitignored = true, -- optional: still hide gitignored files
+        },
     },
-  },
+    window = {
+        mappings = {
+            o = function(state)
+                local node = state.tree.get_node()
+                local path = node.path
+                vim.fn.jobstart({ "xdg-open". path }, { detach = true })
+            end,
+        },
+    },
 })
 
 vim.keymap.set('n', '<leader>n', ':Neotree toggle<CR>', { silent = true })
