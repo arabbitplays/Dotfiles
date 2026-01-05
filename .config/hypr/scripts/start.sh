de65378a86111b9d96462f0fd1e1e6c5ba420c3c
Oschdi <osterjakob@gmail.com>
2026-01-05 22:54:21 +0100

    sync Mo 5. Jan 22:54:11 CET 2026

--- .config/hypr/scripts/start.sh
+++ .config/hypr/scripts/start.sh
@@ -17,6 +17,7 @@ dunst &
 swww-daemon &
 
 systemctl --user restart desktop-manager.service
+sleep 1 # sometimes this second is needed so that the following commands dont get lost, but i dont understand what the dependency is
 
 DesktopManagerCLI theme forest
 DesktopManagerCLI workspace switch 1
